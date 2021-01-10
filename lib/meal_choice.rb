1477d7ebb14d77958a8886964633e29fa26e6320
Shane R <imspammails@gmail.com>
2021-01-10 03:49:26 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,7 +3,7 @@
 
 # Method with three parameters
 def meal_choice(veg1, veg2, protein="tofu")
-  "What a nutritious meal!"
+  puts "What a nutritious meal!"
   return "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
